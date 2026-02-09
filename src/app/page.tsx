36e32c070707426f2740dc4359c602e0d4a452b2
imnotpossib1e <habit2me@gmail.com>
2026-02-09 09:08:35 +0900

    💄 style: 메인 페이지 UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import LinkButton from '@/components/LinkButton';
+import { PencilLine } from 'lucide-react';
+
 export default function Home() {
-  return <h1>DevWriter</h1>;
+  return (
+    <main className="flex flex-1 flex-col items-center gap-11 l pb-20 pt-44">
+      <h2 className="text-center text-6xl font-extrabold">
+        <span>Generate Tech Blogs</span>
+        <br />
+        <span className="text-purple">In Seconds</span>
+      </h2>
+      <h3 className="text-white/80 font-medium">
+        주제만 입력하세요, AI가 개발자용 기술 블로그 글을 완성합니다.
+      </h3>
+      <div className="flex flex-wrap gap-3">
+        <LinkButton href="/generator" size="lg">
+          Start Generating
+          <PencilLine className="w-5" />
+        </LinkButton>
+        <LinkButton href="/" variant="white" size="lg">
+          View Posts
+        </LinkButton>
+      </div>
+    </main>
+  );
 }
