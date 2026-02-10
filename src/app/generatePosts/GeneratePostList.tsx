3a29c0a0883403c8e7038b04608d4fdba90f7dde
imnotpossib1e <habit2me@gmail.com>
2026-02-10 09:47:34 +0900

    🐳 chore: 포스트 페이지 폴더 구조 형성

---
+++ src/app/generatePosts/GeneratePostList.tsx
@@ -0,0 +1,19 @@
+// export default function GeneratePostList() {
+//   return (
+//     <div>
+//       {generateState?.ok && generateState.item.content && (
+//         <div className="mt-8 p-6 bg-gray-50 rounded-lg text-black">
+//           <h2 className="text-2xl font-bold mb-4">📝 생성된 글</h2>
+//           <div className="prose max-w-none">{generateState.item.content}</div>
+//         </div>
+//       )}
+
+//       {/* ✅ 에러 표시 */}
+//       {generateState?.ok === 0 && (
+//         <div className="p-4 bg-red-100 border border-red-400 text-red-700 rounded">
+//           {generateState.message}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
---
+++ src/app/generatePosts/layout.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { ArrowLeft, Sparkles } from 'lucide-react';
+import Link from 'next/link';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col mx-auto w-5xl pt-15 gap-6">
+      <Link href="/" className="flex flex-wrap gap-4 font-bold text-gray-400">
+        <ArrowLeft className="w-5" />
+        Back to Home
+      </Link>
+      <div className="flex flex-wrap gap-4 items-center">
+        <div className="flex justify-center items-center h-12.5 w-12.5 rounded-lg bg-[#252159]">
+          <Sparkles className="text-purple  " />
+        </div>
+        <div className="flex flex-col font-bold">
+          <h2 className="text-2xl">Blog Generator</h2>
+          <span className="text-gray-400">결과는?</span>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+}
