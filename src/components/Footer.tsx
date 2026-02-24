8355dee9f98a6965df177fad88ab552a5d6d23ac
imnotpossib1e <habit2me@gmail.com>
2026-02-24 17:54:38 +0900

    💄 style: 푸터 반응형 UI

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,13 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-bg-dark-blue w-full h-20 mt-10">
-      <span className="flex items-center justify-center h-20 text-white/50">
-        © 2026 DevWriterAI. Generate technical content with AI.
-      </span>
+    <footer className="bg-bg-dark-blue h-20 md:h-30 w-full mt-10">
+      <div className="flex flex-col justify-center items-center w-full h-full text-white/50 text-xs md:text-base">
+        <p>© 2026. DevWriterAI All rights reserved.</p>
+        <p>
+          이 웹 사이트는 멋쟁이사자처럼 로켓단 인턴십 프로젝트로 제작되었습니다.
+        </p>
+      </div>
     </footer>
   );
 }
